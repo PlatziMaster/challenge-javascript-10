f572f1496d13c5e4aae2a3be610fe41d7d6c7ffa
Alexis Araujo <ala1289@gmail.com>
2020-03-30 19:30:21 -0600

    Challenge Solved

--- src/_test_/demo6.test.js
+++ src/_test_/demo6.test.js
@@ -1,21 +1,25 @@
-const { getMaxNumber, getMinNumber } = require ('../demo6');
+const { getMaxNumber, getMinNumber, getAvg } = require('../demo6');
 
 describe('getMaxNumber', () => {
-
   it('should return the max number', () => {
-    expect(getMaxNumber([1,2,3,4,5,6,8,9])).toBe(9);
-    expect(getMaxNumber([1,2,3,4,5,6,8,9,0,100])).toBe(100);
-    expect(getMaxNumber([1,2,3,4,5,-66,8,9,0,34])).toBe(34);
+    expect(getMaxNumber([1, 2, 3, 4, 5, 6, 8, 9])).toBe(9);
+    expect(getMaxNumber([1, 2, 3, 4, 5, 6, 8, 9, 0, 100])).toBe(100);
+    expect(getMaxNumber([1, 2, 3, 4, 5, -66, 8, 9, 0, 34])).toBe(34);
   });
-
 });
 
 describe('getMinNumber', () => {
-
   it('should return the min number', () => {
-    expect(getMinNumber([1,2,3,4,5,6,8,9])).toBe(1);
-    expect(getMinNumber([1,2,3,4,5,6,8,9,0,100])).toBe(0);
-    expect(getMinNumber([1,2,3,4,5,-66,8,9,0,34])).toBe(-66);
+    expect(getMinNumber([1, 2, 3, 4, 5, 6, 8, 9])).toBe(1);
+    expect(getMinNumber([1, 2, 3, 4, 5, 6, 8, 9, 0, 100])).toBe(0);
+    expect(getMinNumber([1, 2, 3, 4, 5, -66, 8, 9, 0, 34])).toBe(-66);
   });
+});
 
+describe('getAvg', () => {
+  it('should return the average', () => {
+    expect(getAvg([10, 10, 10])).toBe(10);
+    expect(getAvg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toBe(5.5);
+    expect(getAvg([10, 20, 30])).toBe(20);
+  });
 });
--- src/demo6.js
+++ src/demo6.js
@@ -1,9 +1,26 @@
-const getMaxNumber = (numbersArray) => {
-  return numbersArray.reduce((num, value)=> value > num ? value : num);
+const faker = require('faker');
+
+const numbersArray = [];
+
+for (let index = 0; index < 1000; index++) {
+  numbersArray.push({
+    number: faker.random.number(1000),
+  });
 }
 
-const getMinNumber = (numbersArray) => {
-  return numbersArray.reduce((num, value)=> value < num ? value : num);
-}
+const getMaxNumber = numbersArray => {
+  return numbersArray.reduce((num, value) => (value > num ? value : num));
+};
 
-module.exports = { getMaxNumber, getMinNumber };
+const getMinNumber = numbersArray => {
+  return numbersArray.reduce((num, value) => (value < num ? value : num));
+};
+
+const getAvg = numbersArray => {
+  return numbersArray.reduce(
+    (sum, value) => sum + value / numbersArray.length,
+    0,
+  );
+};
+
+module.exports = { getMaxNumber, getMinNumber, getAvg };
