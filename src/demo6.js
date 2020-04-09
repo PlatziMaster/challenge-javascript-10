0afa02884ae2d587b793ff952734a46df0de20e1
J. Alvarez <jcalvarezj@gmail.com>
2020-04-09 12:42:15 -0500

    Solved Challenge

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,33 @@
-const getMaxNumber = (numbersArray) => {
-  // your code here
-}
+const getMaxNumber = (numbersArray) => numbersArray.reduce((maximum, number) => {
+  if (number > maximum)
+    maximum = number;
+  return maximum;
+}, Number.MIN_VALUE);
 
-const getMinNumber = (numbersArray) => {
-  // your code here
-}
+const getMinNumber = (numbersArray) => numbersArray.reduce((minimum, number) => {
+  if (number < minimum)
+    minimum = number;
+  return minimum;
+}, Number.MAX_VALUE);
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const total = numbersArray.reduce((total, number) => total + number, 0);
+  return total/numbersArray.length;
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
+
+// Testing with faker
+
+const faker = require('faker')
+
+let testData = [];
+
+for (let i = 0; i < 10; i++)
+  testData.push(faker.random.number());
+
+console.log('Generated array:');
+console.log(testData);
+console.log(`Average: ${getAverage(testData)}`);
+console.log(`Minimum: ${getMinNumber(testData)}`);
+console.log(`Maximum: ${getMaxNumber(testData)}`);
