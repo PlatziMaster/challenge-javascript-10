7afa1341da5c7ef26014a2fc75305005da272b20
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-04-12 22:37:09 -0500

    Resolviendo el challenge de javascript 10

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,37 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  const maxNumber = numbersArray.reduce((count, value) => {
+    return Math.max(count, value);
+  });
+  return maxNumber;
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  const minNumber = numbersArray.reduce((count, value) => {
+    return Math.min(count, value);
+  });
+  return minNumber;
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const sum = numbersArray.reduce((count, value) => {
+    return count + value;
+  });
+  return sum / numbersArray.length;
 }
+
+// Bonus
+const faker = require('faker');
+const sales = [];
+for (let index = 0; index < 1000; index++) {
+  sales.push({
+    price: parseInt(faker.commerce.price(), 10),
+  });
+}
+const amount = sales
+  .map(sale => sale.price) // Descuento del 20%
+  .filter(sale => sale % 2 === 0) // Solo pagaran las cuentas que sean numero pares
+  .reduce((count, price) => count + price, 0); // Sumamos las ventas
+
+console.log(amount); // Total de ventas
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
