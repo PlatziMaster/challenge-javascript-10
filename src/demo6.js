72e3da4d1fb01b518673a137916d7e540b84ca05
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-05-04 17:26:17 -0500

    challenge accomplished

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,30 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  
+  const maxNumber = numbersArray.reduce( (last_value,new_value) => {
+    if (last_value < new_value ){
+      return new_value
+    } else {
+      return last_value;
+    }
+  });
+  return maxNumber;
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  const minNumber = numbersArray.reduce( (last_value,new_value) => {
+    if (last_value > new_value ){
+      return new_value
+    } else {
+      return last_value;
+    }
+  });
+  return minNumber;
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const amount = numbersArray.reduce( (last_value,new_value) => last_value + new_value);
+  const averrage = amount/(numbersArray.length);
+  return averrage;
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
