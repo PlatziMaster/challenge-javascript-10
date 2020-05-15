baed09d106e0c775c0a773c8deb90ec5c17d852d
Jonatan Lazo <jonatan.lazo@lertait.com>
2020-05-15 12:10:51 -0500

    Se implementan las funciones establecidas

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,13 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
-}
+  return numbersArray.reduce((min, value) => Math.max(min, value));
+};
 
 const getMinNumber = (numbersArray) => {
-  // your code here
-}
+  return numbersArray.reduce((min, value) => Math.min(min, value));
+};
 
 const getAverage = (numbersArray) => {
-  // your code here
-}
+  return numbersArray.reduce((a, b) => a + b) / numbersArray.length;
+};
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
