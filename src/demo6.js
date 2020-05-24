2880d7946442a57c25d5c85f556988aba69841b7
Angelica Landazabal <angelica.informatik@gmail.com>
2020-05-24 17:07:40 -0500

    Challenge completed

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,51 @@
+const randomValues = () =>{
+
+  const faker = require('faker')
+  let arr = []  
+
+  for( i = 0; i < 1000; i++){
+    arr.push(faker.random.number())
+  }
+
+  return arr;
+}
+
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  const reducer = (acc, cVal) => {
+    acc = ( acc === undefined || cVal > acc ) ? cVal : acc
+    
+    return acc;
+  }
+  
+  return numbersArray.reduce(reducer);
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  const reducer = (acc, cVal) => {
+    acc = ( acc === undefined || cVal < acc ) ? cVal : acc
+    
+    return acc;
+  }
+
+  return numbersArray.reduce(reducer);
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const reducer = (acc, cVal) => {
+    acc += cVal
+    
+    return acc;
+  }
+
+  const average = numbersArray.reduce(reducer)/numbersArray.length;
+
+  return average;
 }
+
+arr = randomValues();
+console.log(arr);
+console.log(getMaxNumber(arr));
+console.log(getMinNumber(arr));
+console.log(getAverage(arr));
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
