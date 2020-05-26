9c7eb158eb5e180140c59c57709f742f1b785ada
Diego Valdez <dgogo9@hotmail.com>
2020-05-26 18:39:07 -0500

    Solucion Diego Valdez

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,27 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((count, item) => Math.max(count, item))
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((count, item) => Math.min(count, item))
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((count, item) => (count + item)) / numbersArray.length;
 }
+
+
+//Plus: Crear un array de 1000 números y probar las funciones
+
+const faker = require('faker');
+
+const orders = [];
+for (let index = 0; index < 1000; index++) {
+  orders.push(parseInt(faker.random.number(10)));
+}
+
+console.log(getMaxNumber(orders))
+console.log(getMinNumber(orders))
+console.log(getAverage(orders))
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
