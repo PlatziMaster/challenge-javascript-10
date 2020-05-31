047f4dbf6f9baaa29f5e8b3d196102a9aaea2473
Jedp <jedp082@gmail.com>
2020-05-31 15:06:33 -0500

    Challenge done

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,21 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  const reducer = (a,b) => a > b ? a : b;
+ return numbersArray.reduce(reducer);
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  const reducer = (a,b) => a < b ? a : b;
+  console.log(numbersArray.reduce(reducer));
+  return numbersArray.reduce(reducer);
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const reducer = (a,b) => {
+    let avrg = a += b;
+    return avrg;
+  }
+  return numbersArray.reduce(reducer)/numbersArray.length;
 }
+
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
