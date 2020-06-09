125879996326cbebcc53811bab5ee03110fdd4eb
narias1999 <nicolasarias870@gmail.com>
2020-06-08 23:12:16 -0500

    challenge solved

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,16 @@
-const getMaxNumber = (numbersArray) => {
-  // your code here
-}
+const getMaxNumber = (numbersArray) =>
+  numbersArray.reduce(
+    (acc, current) => (acc > current ? acc : current),
+    -Infinity
+  );
 
-const getMinNumber = (numbersArray) => {
-  // your code here
-}
+const getMinNumber = (numbersArray) =>
+  numbersArray.reduce(
+    (acc, current) => (acc < current ? acc : current),
+    Infinity
+  );
 
-const getAverage = (numbersArray) => {
-  // your code here
-}
+const getAverage = (numbersArray) =>
+  numbersArray.reduce((acc, current) => acc + current, 0) / numbersArray.length;
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
