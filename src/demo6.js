fe2207f30cc8836e3c8b83ebab0417b5fc04abf9
jromeroc <jcamiloromeroc@gmail.com>
2020-07-07 22:35:44 -0500

    Aplicando las sugerencias :)

--- src/demo6.js
+++ src/demo6.js
@@ -1,16 +1,14 @@
 const getMaxNumber = (numbersArray) => {
-  let max = numbersArray.reduce((acc, val) => val > acc ? val: acc); 
-  return max
+  let currentNumber = numbersArray.reduce((acc, val) => val > acc ? val: acc); 
+  return currentNumber
 }
 
 const getMinNumber = (numbersArray) => {
-  let min = numbersArray.reduce((acc, val) => val < acc ? val: acc); 
-  return min
+  return numbersArray.reduce((acc, val) => val < acc ? val: acc); 
 }
 
 const getAverage = (numbersArray) => {
-  let avg = numbersArray.reduce((acc, val)=> acc + val);
-  return avg/numbersArray.length;
+  return numbersArray.reduce((acc, val)=> acc + val)/numbersArray.length;
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
