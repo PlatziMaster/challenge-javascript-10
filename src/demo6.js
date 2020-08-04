cc7124f5d7cff96dbe040e293efbf577fa81be95
juliodev <juliocesardeveloper@gmail.com>
2020-08-04 16:21:10 -0500

    Resueltos los primeros tres retos

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,25 @@
+const faker = require('faker');
+
+const randomNumbersArray = [];
+const total = 1000;
+for (let index = 0; index < total; index++) {
+  randomNumbersArray.push(faker.random.number(total));
+};
+console.log(randomNumbersArray);
+
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((accumulator, nextValue) => Math.max(accumulator, nextValue));
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((accumulator, nextValue) => Math.min(accumulator, nextValue));
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  const average =  numbersArray.reduce(function(accumulator, nextValue){
+    return (accumulator + nextValue);
+  }, 0);
+  return average / numbersArray.length;
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
