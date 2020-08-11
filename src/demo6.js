735c2c57eb65d4fa2a6a1b42a40a2efffb4fa204
johan-avila <johan-avila@outlook.com>
2020-08-11 00:16:52 -0500

    reto completado

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,36 @@
-const getMaxNumber = (numbersArray) => {
+const getMaxNumber = (numbersArray ) => {
   // your code here
+  return numbersArray.reduce((acum, curr) => {
+    if (acum < curr) {
+      acum = curr
+    }
+    return acum
+  })
 }
 
-const getMinNumber = (numbersArray) => {
+const getMinNumber = (numbersArray= [1, 2, 3, 20, 1, 0, 2]) => {
   // your code here
+  return numbersArray.reduce((acum, curr) => {
+    if (acum > curr) {
+      acum = curr
+
+    }
+    return acum
+  })
 }
 
 const getAverage = (numbersArray) => {
   // your code here
+  let total = numbersArray.reduce((acum, curr) => {
+    return acum + curr
+  })
+
+  let promedio = total / numbersArray.length
+  return promedio
 }
 
-module.exports = { getMaxNumber, getMinNumber, getAverage };
+module.exports = {
+  getMaxNumber,
+  getMinNumber,
+  getAverage
+};
