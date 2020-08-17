94fdba8663529d25b1ebee210fb79fd7cc38399b
fercc1097 <fercc1097@gmail.com>
2020-08-17 04:23:56 -0500

    challenge accepted

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,27 @@
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce(maxNumber);
+}
+function maxNumber(total, num) {
+  let mayor;
+  mayor = total > num ? total : num;
+  return mayor;
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce(minNumber);
+}
+function minNumber(total, num) {
+  let min;
+  min = total < num ? total : num;
+  return min;
 }
 
 const getAverage = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce(addNumber)/numbersArray.length;
 }
+function addNumber(total, num) {
+  return total+num;
+}
+
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
