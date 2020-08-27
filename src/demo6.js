bed63ed90b2cdc2ff82746a3e3cfe95570c62e2a
juan sebastian herrera <juan.herreraca@campusucc.edu.co>
2020-08-26 21:37:53 -0500

    Challenge solved

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,54 @@
+
+
+let numbersArray = [ 2, 4, 6, 8];
+
+
 const getMaxNumber = (numbersArray) => {
-  // your code here
+  return numberMaximo = numbersArray.reduce((max, number) => {
+    if (max, number){
+      max = number
+    }
+    return max
+  });
 }
 
 const getMinNumber = (numbersArray) => {
-  // your code here
+  return numbersArray.reduce((min, number) => {
+    if (min, number){
+      min = number
+    }
+    return min
+  });
 }
+
 
 const getAverage = (numbersArray) => {
-  // your code here
+  let suma = numbersArray.reduce((average, number) => {
+    return average + number
+  });
+  let average = suma / numbersArray.length
+  return average
 }
 
+getMaxNumber(numbersArray);
+console.log(numberMaximo)
+
+
 module.exports = { getMaxNumber, getMinNumber, getAverage };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
