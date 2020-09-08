ee031b54ab8b56bfb9bc4f4c2de3e77a0cfe5a8c
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-09-08 00:12:16 -0500

    Solving the challenge

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,29 @@
 const getMaxNumber = (numbersArray) => {
   // your code here
+  console.log(numbersArray);
+  let maxNumber = numbersArray.reduce((a, b) => Math.max (a, b));
+  console.log(maxNumber);
 }
 
 const getMinNumber = (numbersArray) => {
   // your code here
+  console.log(numbersArray);
+  let minNumber = numbersArray.reduce((a, b) => Math.min (a, b));
+  console.log(minNumber);
 }
 
 const getAverage = (numbersArray) => {
   // your code here
+  console.log(numbersArray);
+  let avgNumber = numbersArray.reduce((a, b) => (a + b)) / numbersArray.length;
+  console.log(avgNumber);
 }
+
+
+getMaxNumber([2,4,5,6]);
+getMinNumber([2,4,5,6]);
+getAverage([10,10,10]);
+
+
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
