97078e9893d58b1ac5a0ce4aabec7ec0ef2f1913
jonathanrchamorro <68171390+jonathanrchamorro@users.noreply.github.com>
2020-09-12 14:40:23 -0500

    Se cambia el archivo demo6

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,30 @@
 const getMaxNumber = (numbersArray) => {
   // your code here
+  return numbersArray.reduce((max, numero) => {
+    if(max < numero) {
+      max = numero;
+    }
+    return max;
+});
 }
 
 const getMinNumber = (numbersArray) => {
   // your code here
+  return numbersArray.reduce((menor, numero) => {
+    if(menor > numero) {
+      menor = numero;
+    }
+    return menor;
+});
 }
 
 const getAverage = (numbersArray) => {
   // your code here
+  let sumatoria = numbersArray.reduce((average, numero) => {
+    return average + numero  
+});
+  let average = sumatoria / numbersArray.length
+    return average
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
