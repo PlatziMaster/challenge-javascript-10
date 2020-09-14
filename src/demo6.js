eb9be84aa8868c596c0f9e6a056485fe3eee1995
jonathanrchamorro <68171390+jonathanrchamorro@users.noreply.github.com>
2020-09-14 09:56:51 -0500

    Agrego comentarios al codigo

--- src/demo6.js
+++ src/demo6.js
@@ -1,5 +1,16 @@
+// RETO 1
+// Usando la función reduce debes buscar el número más pequeño y el más grande.
+
+// Pasos
+// Editar el archivo demo6.js
+// Usando reduce encontrar el numero más pequeño.
+// Usando reduce encontrar el numero más grande.
+// Obtener el promedio de un array.
+
+
 const getMaxNumber = (numbersArray) => {
   // your code here
+  //imprime el numero mayor
   return numbersArray.reduce((max, numero) => {
     if(max < numero) {
       max = numero;
@@ -7,9 +18,10 @@ const getMaxNumber = (numbersArray) => {
     return max;
 });
 }
-
+//------------------------------------------------------//
 const getMinNumber = (numbersArray) => {
   // your code here
+  //Imprime el numero menor
   return numbersArray.reduce((menor, numero) => {
     if(menor > numero) {
       menor = numero;
@@ -17,14 +29,14 @@ const getMinNumber = (numbersArray) => {
     return menor;
 });
 }
-
+//------------------------------------------------------//
 const getAverage = (numbersArray) => {
   // your code here
-  let sumatoria = numbersArray.reduce((average, numero) => {
+  //Promedio de Array
+  let suma = numbersArray.reduce((average, numero) => {
     return average + numero  
 });
-  let average = sumatoria / numbersArray.length
+  let average = suma/ numbersArray.length
     return average
 }
-
-module.exports = { getMaxNumber, getMinNumber, getAverage };
+module.exports = { getMaxNumber, getMinNumber, getAverage }; 
