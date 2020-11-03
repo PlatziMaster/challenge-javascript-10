1906752bf59983225be41debd1c676a5760ac8f4
Daniel Saavedra <huyno50@gmail.com>
2020-11-03 01:08:14 -0500

    challenge-10-solved-v0.1

--- src/demo6.js
+++ src/demo6.js
@@ -1,13 +1,28 @@
 const getMaxNumber = (numbersArray) => {
   // your code here
+  return numbersArray.reduce((previosValue, currentValue) => {
+    if (previosValue > currentValue){
+      return previosValue;
+    }
+    return currentValue;
+  })
 }
 
 const getMinNumber = (numbersArray) => {
   // your code here
+  return numbersArray.reduce((previosValue, currentValue) => {
+    if (previosValue < currentValue){
+      return previosValue;
+    }
+    return currentValue;
+  })
 }
 
 const getAverage = (numbersArray) => {
   // your code here
+  const rta = numbersArray.reduce((previosValue, currentValue) => {
+    return previosValue + currentValue;
+  })
 }
 
 module.exports = { getMaxNumber, getMinNumber, getAverage };
